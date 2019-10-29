9149c72973c9a77849c186d84bdb46a9eb646987
mohammed <ibrahimdamy@gmail.com>
2019-10-29 21:18:28 +0100

    token and bcrypt

--- src/server.js
+++ src/server.js
@@ -17,6 +17,14 @@ app.use(bodyParser.json({ extended : true }));
 // app router
 app.use('/api/v1/', userRouter);
 
+// wronge routes
+app.use('*', () => {
+    resizeBy.status(404).json({
+        status: 'error',
+        error: 'incorrect route'
+    })
+})
+
 app.listen(port,() => {
     console.log(`app is running on ${port}`)
 })
