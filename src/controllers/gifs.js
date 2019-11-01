08438a5ecf58c11d92dc63651fd34166e562495b
mohammed <ibrahimdamy@gmail.com>
2019-11-01 16:48:37 +0100

    feature(post)

--- src/controllers/gifs.js
+++ src/controllers/gifs.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import cloudinary from 'cloudinary';
 import { image } from 'cloudinary/lib-es5/cloudinary';
+import pool from '../models/database';
 
 // configure cloudinary
 
@@ -16,46 +17,48 @@ const gifController = {
         let image = req.files.gif;
         const { title, authorId } = req.body;
         try {
-            jwt.verify(req.token, process.env.SECRET_KEY, (err, data) => {
-                // if(err) {
-                //     res.status(403).json({
-                //         status: 'error',
-                //         error: 'incorrect token'
-                //     })
-                // }
+            jwt.verify(req.token, process.env.SECRET_KEY, async (err, data) => {
+                if (!image || !title || !authorId) {
+                    return res.status(400).json({
+                        status: 'error',
+                        error: 'all fields are required'
+                    });
+                };
 
-                
-                cloudinary.v2.uploader.upload(image.tempFilePath, {resourse_type : 'gif'}, (err, result, imageUrl) => {
-                    if(err) {
-                        console.log(err)
+                if(err) {
+                    return res.status(403).json({
+                        status: 'error',
+                        error: 'incorrect token'
+                    })
+                }
+
+
+                cloudinary.v2.uploader.upload(image.tempFilePath, {resourse_type : 'gif'})
+                .then(async (result) =>  {
+                const gif = `INSERT INTO gifs (image, title, authorId , createdOn)
+            VALUES($1, $2, $3, $4) RETURNING *`;
+            const values = [result.url, title, authorId, new Date().toLocaleDateString()];
+            const gifQuery = await pool.query(gif, values)
+
+                res.status(201).json({
+                    status: 'success',
+                    data:  {
+                        gifId: gifQuery.rows[0].gifid,
+                        message: '',
+                        createdOn: gifQuery.rows[0].createdon,
+                        title: gifQuery.rows[0].title,
+                        imageUrl: gifQuery.rows[0].image
                     }
-                     imageUrl = result.url
-                    return imageUrl
                 })
-
-
-                // const cloudinaryLink = cloudinary.v2.uploader.upload(image.tempFilePath, {resourse_type : 'gif'})
-                // .then((res) =>  
-                // res
-                // // console.log(res)
-                // ).catch((e) =>
-                //     console.log(e) 
-                // )
-                // // console.log(data)
-                // console.log(cloudinaryLink)
-
-                // const gif = `INSERT INTO gifs (image, title, userId , createdOn)
-            // VALUES($1, $2, $3, $4) RETURNING *`;
-            // const values = [title, userId, new Date().toLocaleDateString()]
-
-
+                }).catch((e) =>
+                    console.log(e) 
+                )
             })
    
         }
         catch (e) {
             console.log(e);
-        }
-        
+        }    
     }
 
 }
