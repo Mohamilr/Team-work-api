6bc4f855161f7536bca2eeb475400de442e4f176
mohammed <ibrahimdamy@gmail.com>
2019-11-01 16:58:12 +0100

    delete gif

--- src/routes/gif.route.js
+++ src/routes/gif.route.js
@@ -7,7 +7,7 @@ import verify from '../controllers/sign-login';
 
 const gifRouter = Router();
 
-gifRouter.post('/gifs', verify.verifyToken ,gifController.postGif)
-
+gifRouter.post('/gifs', verify.verifyToken ,gifController.postGif);
+gifRouter.delete('/gifs/:id', verify.verifyToken, gifController.deleteGif);
 
 export default gifRouter;
