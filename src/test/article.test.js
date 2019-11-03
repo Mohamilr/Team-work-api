6b1a7822293e76073a2a6db580ad8bd0309c6697
mohammed <ibrahimdamy@gmail.com>
2019-11-03 05:06:31 +0100

    chore(test create article route)

--- src/test/article.test.js
+++ src/test/article.test.js
@@ -4,26 +4,68 @@ import chaiHttp from 'chai-http';
 import app from '../server';
 
 // token
-import logInToken from './register.test';
+import token from './register.test';
 
 chai.use(chaiHttp);
 chai.should();
 
-describe.skip('articles', () => {
-    describe('POST create article', () => {
-        it('should create a new article', (done) => {
-            chai.request(app)
-            .post('/api/v1/articles')
-            .set( 'Authorization', `bearer ${logInToken}` )
-            .send({
-                title: 'my first official project',
-                article: 'i was an intern at Hotels.ng.',
-                authorId: 1
+describe('articles', () => {
+    describe('create article', () => {
+            // error om wrong token
+        describe('POST create article', () => {
+            it('should give error on wrong token', (done) => {
+                chai.request(app)
+                .post('/api/v1/articles')
+                .set('Authorization', `bearer wrong token`)
+                .send({
+                    title: 'my first official project',
+                    article: 'i was an intern at Hotels.ng.',
+                    authorId: 1
+                })
+                .end((err, res) => {
+                    res.should.have.status(403)
+                })
+                done();
+                console.log(token)
             })
-            .end((err, res) => {
-                res.should.have.status(201)
+        })
+    
+        // error on empty body values
+        describe('POST create article', () => {
+            it('should give error on empty body values', (done) => {
+                chai.request(app)
+                .post('/api/v1/articles')
+                .set('Authorization', `bearer wrong token`)
+                .send({
+                    title: 'my first official project',
+                    article: '',
+                    authorId: 1
+                })
+                .end((err, res) => {
+                    res.should.have.status(400)
+                })
+                done();
+                console.log(token)
             })
-            done();
+        })
+    
+        describe('POST create article', () => {
+            it('should create a new article', (done) => {
+                chai.request(app)
+                .post('/api/v1/articles')
+                .set('Authorization', `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vaGFtbWVkIiwicGFzc3dvcmQiOiJpYnJhaGltIiwiaWF0IjoxNTcyNzUyNjE1LCJleHAiOjE1NzI4MzkwMTV9.aNUBP0nNhk5etK-Fb98UzDQOZH1sPIrnbFsAEsRiAVo`)
+                .send({
+                    title: 'my first official project',
+                    article: 'i was an intern at Hotels.ng.',
+                    authorId: 1
+                })
+                .end((err, res) => {
+                    res.should.have.status(201)
+                })
+                done();
+                console.log(token)
+            })
         })
     })
+    
 })
