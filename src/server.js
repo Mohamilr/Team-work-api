418c30e532328415de40983461f7b7e9c82716de
mohammed <ibrahimdamy7@gmail.com>
2019-11-14 12:03:20 +0100

    bug(cors): fix cors errors-1

--- src/server.js
+++ src/server.js
@@ -34,6 +34,18 @@ app.use(fileUpload({
 }))
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+  
+    next();
+})
+
 // app router
 app.use('/api/v1/', userRouter);
 app.use('/api/v1/', articleRouter);
