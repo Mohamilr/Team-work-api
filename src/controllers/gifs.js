3d45188fad269c5596742dd01a5e693d8e876e77
mohammed <ibrahimdamy7@gmail.com>
2019-12-19 19:00:02 +0100

    ch(refactor): refactor gif controller

--- src/controllers/gifs.js
+++ src/controllers/gifs.js
@@ -1,6 +1,6 @@
-import jwt from 'jsonwebtoken';
 import cloudinary from 'cloudinary';
 import pool from '../models/database';
+import jsonResponse from '../helpers/jsonResponse';
 
 // import cloudinary
 import cloudinaryConfig from '../config/cloudinary.config';
@@ -8,27 +8,14 @@ import cloudinaryConfig from '../config/cloudinary.config';
 
 
 const gifController = {
-    postGif(req, res) {
+    async postGif(req, res) {
         //  gif key (gif) form-data
         let image = req.files.gif;
         const { gifTitle, gifAuthorId } = req.body;
         try {
-            // verify token
-            jwt.verify(req.token, process.env.SECRET_KEY, async (err, data) => {
-                // incorrect token
-                if (err) {
-                    return res.status(403).json({
-                        status: 'error',
-                        error: 'incorrect token'
-                    });
-                };
-
                 // empty body values (form-data)
                 if (!image || !gifTitle || !gifAuthorId) {
-                    return res.status(400).json({
-                        status: 'error',
-                        error: 'all fields are required'
-                    });
+                    return jsonResponse(res, 'error', 400, 'all fields are required');
                 };
 
                 // cloudinary upload
@@ -41,53 +28,33 @@ const gifController = {
                         const gifQuery = await pool.query(gif, values);
 
                         // post gif response 
-                        res.status(201).json({
-                            status: 'success',
-                            data: {
-                                gifId: gifQuery.rows[0].gifid,
-                                message: 'gif image successfully posted',
-                                createdOn: gifQuery.rows[0].createdon,
-                                title: gifQuery.rows[0].title,
-                                imageUrl: gifQuery.rows[0].image
-                            }
+                        return jsonResponse(res, 'success', 201, {
+                            gifId: gifQuery.rows[0].gifid,
+                            message: 'gif image successfully posted',
+                            createdOn: gifQuery.rows[0].createdon,
+                            title: gifQuery.rows[0].title,
+                            imageUrl: gifQuery.rows[0].image
                         });
                     })
                     .catch((e) =>
                         console.log(e)
                     )
-            })
         }
         catch (e) {
             console.log(e);
         };
     },
-    deleteGif(req, res) {
+    async deleteGif(req, res) {
         // parameter (number)
         const id = parseInt(req.params.id);
         try {
-            // verify token
-            jwt.verify(req.token, process.env.SECRET_KEY, async (err, data) => {
-                // incorrect token
-                if (err) {
-                    return res.status(403).json({
-                        status: 'error',
-                        error: 'incorrect token'
-                    });
-                };
-
                 // delete gif query
                 const deleteGif = `DELETE FROM gifs WHERE gifId=$1`;
                 const value = [id];
                 const deleteGifQuery = await pool.query(deleteGif, value);
 
                 // delete gif response
-                res.status(200).json({
-                    status: 'success',
-                    data: {
-                        message: 'gif post successfully deleted'
-                    }
-                });
-            });
+                return jsonResponse(res, 'success', 200, 'gif post successfully deleted');
         }
         catch (e) {
             console.log(e)
